5837645800ef724b4c89ca1430f9683dbd3c6d7e
AbdulazizMualla <amgad74@hotmail.com>
2021-05-05 04:12:55 +0300

    crate route and function log in

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -23,10 +24,29 @@ class AuthController extends Controller
         if ($user){
             $accessToken = $user->createToken('authToken')->accessToken;
             if ($accessToken){
-                return ['user' => $user , 'accessToken' => $accessToken];
+                return ['user' =>  new UserResource($user) , 'access_token' => $accessToken];
             }
             return response()->json(['message' => 'Error pleas try aging'] , 500);
         }
+        return response()->json(['message' => 'Error pleas try aging'] , 500);
+    }
+
+    public function login(Request $request)
+    {
+        $validateData = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($validateData)){
+            return response()->json(['message' => 'invalid login details'] , 401);
+        }
+
+        $accessToken = auth()->user()->crateToken('authToken')->accessToken;
+        if ($accessToken){
+            return ['user' =>  new UserResource(auth()->user()->load('profile')) , 'access_token' => $accessToken];
+        }
+
         return response()->json(['message' => 'Error pleas try aging'] , 500);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::post('/register' , 'App\Http\Controllers\Api\AuthController@register');
+Route::post('/login' , 'App\Http\Controllers\Api\AuthController@login');
